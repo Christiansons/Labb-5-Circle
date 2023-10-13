ec52d56f45860b72e952e0c5b9ac1d803a29f86a
Christiansons <alexander.christianson@outlook.com>
2023-10-13 14:16:03 +0200

    Add project files.

---
+++ Circle.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Labb_5___Circles
+{
+	internal class Circle
+	{
+		public double area { get; set; }
+		public double radius { get; set; }
+		public Circle(double radius)
+		{
+			this.radius = radius;
+			area = GetArea(radius);
+		}
+
+		public double GetArea(double radius)
+		{
+			double area = radius * radius * Math.PI;
+
+			return area;
+		}
+	}
+}
---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿namespace Labb_5___Circles
+{
+	internal class Program
+	{
+		static void Main(string[] args)
+		{
+			static void Main(string[] args)
+			{
+				Circle circle1 = new Circle(5);
+
+				Console.WriteLine("The approximate area for a circle with {0} radius is {1:.###}^2", circle1.radius, circle1.area);
+
+				Circle circle2 = new Circle(6);
+
+				Console.WriteLine("The approximate area for a circle with {0} radius is {1:.###}^2", circle2.radius, circle2.area);
+			}
+		}
+	}
+}
